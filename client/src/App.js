f04c24cae5d46e1675cd96d904b02417e01ba76a
phukon <rikiphukon16@gmail.com>
2023-02-23 14:19:06 +0530

    created routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,18 @@
 import './App.css';
 import Post from './Post';
+import Header from './Header';
+import {Route, Routes} from 'react-router-dom';
+import Layout from './Layout';
 
 
 function App() {
   return (
-    <main>
-      <header>
-        <a href = "#" className="logo">MyBlog</a>
-        <nav>
-          <a href = "#">Login</a>
-          <a href = "#">Register</a>
-        </nav>
-      </header>
-      <Post/>
-      <Post/>
-      <Post/>
-    </main>
+    <Routes>
+      <Route path='/' element={<Layout/>}>
+        <Route index element={<Post/>}/>
+        <Route path={'/login'} element = {<div>login page</div>}/>
+      </Route>
+    </Routes>
   );
 }
 
