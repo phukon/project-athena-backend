5df63d4f7287d189dcd341304eb94a5617ac5963
phukon <rikiphukon16@gmail.com>
2023-02-23 13:47:27 +0530

    intalled react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,6 @@
 import './App.css';
+import Post from './Post';
+
 
 function App() {
   return (
@@ -10,45 +12,9 @@ function App() {
           <a href = "#">Register</a>
         </nav>
       </header>
-      <div className='post'>
-        <div className="image">
-          <img src="https://avatars.githubusercontent.com/u/60285613?v=4"></img>
-        </div>
-        <div className="texts">
-          <h2>Riki Phukon makes an LMS</h2>
-          <p className="info">
-            <a href="#" className="author">Riki Phukon</a>
-            <time>2023-02-23 13:04</time>
-          </p>
-          <p className="summary">The hours have grown long and the night has grown deep, but my mind is still racing with thoughts and ideas. ng with thoughts and ideas. ng with thoughts and ideas. ng with thoughts and ideas. ng with thoughts and ideas. ng with thoughts and ideas.</p>
-        </div>
-      </div>
-      <div className='post'>
-        <div className="image">
-          <img src="https://avatars.githubusercontent.com/u/60285613?v=4"></img>
-        </div>
-        <div className="texts">
-          <h2>Riki Phukon makes an LMS</h2>
-          <p className="info">
-            <a href="#" className="author">Riki Phukon</a>
-            <time>2023-02-23 13:04</time>
-          </p>
-          <p className="summary">The hours have grown long and the night has grown deep, but my mind is still racing with thoughts and ideas. ng with thoughts and ideas. ng with thoughts and ideas. ng with thoughts and ideas. ng with thoughts and ideas. ng with thoughts and ideas.</p>
-        </div>
-      </div>
-      <div className='post'>
-        <div className="image">
-          <img src="https://avatars.githubusercontent.com/u/60285613?v=4"></img>
-        </div>
-        <div className="texts">
-          <h2>Riki Phukon makes an LMS</h2>
-          <p className="info">
-            <a href="#" className="author">Riki Phukon</a>
-            <time>2023-02-23 13:04</time>
-          </p>
-          <p className="summary">The hours have grown long and the night has grown deep, but my mind is still racing with thoughts and ideas. ng with thoughts and ideas. ng with thoughts and ideas. ng with thoughts and ideas. ng with thoughts and ideas. ng with thoughts and ideas.</p>
-        </div>
-      </div>
+      <Post/>
+      <Post/>
+      <Post/>
     </main>
   );
 }
---
+++ client/src/Post.js
@@ -0,0 +1,17 @@
+export default function Post() {
+    return (
+        <div className='post'>
+        <div className="image">
+          <img src="https://avatars.githubusercontent.com/u/60285613?v=4"></img>
+        </div>
+        <div className="texts">
+          <h2>Riki Phukon makes an LMS</h2>
+          <p className="info">
+            <a href="#" className="author">Riki Phukon</a>
+            <time>2023-02-23 13:04</time>
+          </p>
+          <p className="summary">The hours have grown long and the night has grown deep, but my mind is still racing with thoughts and ideas. ng with thoughts and ideas. ng with thoughts and ideas. ng with thoughts and ideas. ng with thoughts and ideas. ng with thoughts and ideas.</p>
+        </div>
+      </div>
+    )
+}
