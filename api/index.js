feb84227b819d67db8a56c5a3b2dea3cc2285912
phukon <rikiphukon16@gmail.com>
2023-02-24 00:52:16 +0530

    encrypted passwords

--- api/index.js
+++ api/index.js
@@ -1,17 +1,28 @@
 const express = require('express')
 const app = express()
 const cors = require('cors');
+const { default: mongoose } = require('mongoose');
+const User = require('./models/User')
+const bcrypt = require('bcryptjs');
+
+
+
+
+
+const salt = bcrypt.genSaltSync(10);
 
 app.use(cors())
 app.use(express.json())
 
-app.post('/register', (req, res) => {
+mongoose.connect('mongodb+srv://random:NmlwvZsMBbiSIWmt@cluster0.8i8ngw9.mongodb.net/?retryWrites=true&w=majority')
+
+app.post('/register', async(req, res) => {
     const {username, password} = req.body;
-    res.json({requestData: {username, password}});
+    try{
+        const userDoc = await User.create({username, password:bcrypt.hashSync(password,salt)})
+        res.json(userDoc);
+    } catch(e) {res.status(400).json(e)}
+    
 })
 
 app.listen(4000)
-
-//NmlwvZsMBbiSIWmt
-
-//mongodb+srv://random:NmlwvZsMBbiSIWmt@cluster0.8i8ngw9.mongodb.net/?retryWrites=true&w=majority
--- client/src/pages/RegisterPage.js
+++ client/src/pages/RegisterPage.js
@@ -6,11 +6,16 @@ export default function RegisterPage() {
     const [password, setPassword] = useState('')
     async function register(ev) {
         ev.preventDefault();
-        await fetch('http://localhost:4000/register', {
+        const response = await fetch('http://localhost:4000/register', {
             method: 'POST',
             body: JSON.stringify({username, password}),
             headers: {'Content-Type':'application/json'}
         })
+        if(response.status === 200) {
+            alert('Registration succesful')
+        } else {
+            alert('Registration failed')
+        }
     }
 
 /*This attribute sets an event listener for the "onChange" event of the input field.
