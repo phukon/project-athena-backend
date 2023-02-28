6b8f6b09b818c43c6494374dda9743c6f63db78e
phukon <rikiphukon16@gmail.com>
2023-02-28 23:18:51 +0530

    code restructure

--- api/models/User.js
+++ api/models/User.js
@@ -6,6 +6,6 @@ const UserSchema = new Schema({
   password: {type: String, required: true},
 });
 
-const UserModel = model('User', UserSchema);
+const UserModel = model('Students', UserSchema);
 
 module.exports = UserModel;
--- client/src/Header.js
+++ client/src/Header.js
@@ -1,47 +1,46 @@
-import {useContext, useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-import { UserContext } from "./UserContext";
+import {Link} from "react-router-dom";
+import {useContext, useEffect, useState} from "react";
+import {UserContext} from "./UserContext";
 
 export default function Header() {
-  const {setUserInfo, userInfo} = useContext(UserContext);
-  useEffect(()=> {
+  const {setUserInfo,userInfo} = useContext(UserContext);
+  useEffect(() => {
     fetch('http://localhost:4000/profile', {
-      credentials: 'include'
+      credentials: 'include',
     }).then(response => {
-      response.json().then(userInfo =>{
+      response.json().then(userInfo => {
         setUserInfo(userInfo);
-      })
-    })
-  }, [])
+      });
+    });
+  }, []);
 
-    function logout(){
-      fetch('http://localhost:4000/logout', {
-        credentials: 'include',
-        method: 'POST'
-    })
+  function logout() {
+    fetch('http://localhost:4000/logout', {
+      credentials: 'include',
+      method: 'POST',
+    });
     setUserInfo(null);
-    }
+  }
 
-    const username = userInfo?.username;
+  const username = userInfo?.username;
 
-    return (
-        <header>
-        <Link to = "#" className="logo">MyBlog</Link>
-        <nav>
-          {username && (
-            <>
-              <Link to = "/create">Create new post</Link>
-              <a onClick={logout}>Logout</a>
-            </>
-          )}
-
-          {!username && (
-            <>
-              <Link to = "/login">Login</Link>
-              <Link to = "/register">Register</Link>
-            </>
-          )}
-        </nav>
-      </header>
-    )
+  return (
+    <header>
+      <Link to="/" className="logo">MyBlog</Link>
+      <nav>
+        {username && (
+          <>
+            <Link to="/create">Create new post</Link>
+            <a onClick={logout}>Logout ({username})</a>
+          </>
+        )}
+        {!username && (
+          <>
+            <Link to="/login">Login</Link>
+            <Link to="/register">Register</Link>
+          </>
+        )}
+      </nav>
+    </header>
+  );
 }
