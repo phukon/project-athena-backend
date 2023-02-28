f83447ef1565250f697a77e7bb44c8e8861410c4
phukon <rikiphukon16@gmail.com>
2023-02-28 22:52:37 +0530

    major change

--- api/models/Post.js
+++ api/models/Post.js
@@ -1,13 +1,15 @@
 const mongoose = require('mongoose');
-const {Schema, model} = mongoose;
+const {Schema,model} = mongoose;
 
 const PostSchema = new Schema({
-    title: String,
-    summary: String,
-    content: String,
-    cover: String,
-    author: {type:Schema.Types.ObjectId, ref:'students'}
-}, {timestamps:true})
+  title:String,
+  summary:String,
+  content:String,
+  cover:String,
+  author:{type:Schema.Types.ObjectId, ref:'User'},
+}, {
+  timestamps: true,
+});
 
 const PostModel = model('Post', PostSchema);
 
--- client/src/pages/RegisterPage.js
+++ client/src/pages/RegisterPage.js
@@ -1,35 +1,33 @@
 import {useState} from "react";
 
-
 export default function RegisterPage() {
-    const [username, setUsername] = useState('')
-    const [password, setPassword] = useState('')
-    async function register(ev) {
-        ev.preventDefault();
-        const response = await fetch('http://localhost:4000/register', {
-            method: 'POST',
-            body: JSON.stringify({username, password}),
-            headers: {'Content-Type':'application/json'}
-        })
-        if(response.status === 200) {
-            alert('Registration succesful')
-        } else {
-            alert('Registration failed')
-        }
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  async function register(ev) {
+    ev.preventDefault();
+    const response = await fetch('http://localhost:4000/register', {
+      method: 'POST',
+      body: JSON.stringify({username,password}),
+      headers: {'Content-Type':'application/json'},
+    });
+    if (response.status === 200) {
+      alert('registration successful');
+    } else {
+      alert('registration failed');
     }
-
-/*This attribute sets an event listener for the "onChange" event of the input field.
-When the user changes the value of the input field, this event is triggered and the
-function specified in the attribute is called. In this case, the function updates the
-value of the "username" state variable with the current value of the input field,
-which is obtained from ev.target.value.*/
-
-    return (
-        <form className="register" onSubmit={register}>
-            <h1>Registration Page</h1>
-            <input type="text" placeholder="username" value={username} onChange={ev => setUsername(ev.target.value)}></input>
-            <input type="password" placeholder="password" value={password} onChange={ev => setPassword(ev.target.value)}></input>
-            <button>Register</button>
-        </form> 
-    )
+  }
+  return (
+    <form className="register" onSubmit={register}>
+      <h1>Register</h1>
+      <input type="text"
+             placeholder="username"
+             value={username}
+             onChange={ev => setUsername(ev.target.value)}/>
+      <input type="password"
+             placeholder="password"
+             value={password}
+             onChange={ev => setPassword(ev.target.value)}/>
+      <button>Register</button>
+    </form>
+  );
 }
